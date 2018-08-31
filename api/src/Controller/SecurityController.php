63dcb7a269c4b6d1a9458ecb16d44e76c581becf
Rida Ridane <ext-rida.ridane@tessi.fr>
2018-08-31 16:09:22 +0200

    UNSTABLE Registration - Token not given back when register user

--- api/src/Controller/SecurityController.php
+++ api/src/Controller/SecurityController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class SecurityController extends AbstractController
 {
@@ -48,6 +50,65 @@ class SecurityController extends AbstractController
             'registration/register.html.twig',
             array('form' => $form->createView())
         );
+    }
+
+    public function apiRegistration(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $em = $this->get('doctrine')->getManager();
+//        $logger = $this->container->get('logger');
+
+        $password = $request->request->get('password');
+        $username = $request->request->get('username');
+        $email = $request->request->get('email');
+
+        $user = new User();
+
+        $user->setPassword($passwordEncoder->encodePassword($user, $password));
+        $user->setUsername($username);
+        $user->setEmail($email);
+
+        $entityManager = $this->getDoctrine()->getManager();
+//        $em->persist($user);
+//        $em->flush();
+
+        $jwtManager = $this->container->get('lexik_jwt_authentication.jwt_manager');
+        /** @var EventDispatcher $dispatcher */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $jwt = $jwtManager->create($user);
+        $response = new JsonResponse();
+        $event = new AuthenticationSuccessEvent(array('token' => $jwt), $user, $request);
+        $event->setResponse($response);
+        $dispatcher->dispatch(Events::AUTHENTICATION_SUCCESS, $event);
+        $response->setData($event->getData());
+
+        return $response;
+
+//        return $this->redirectToRoute('api_login_check', array('username' => $username, 'password' => $password));
+
+//        try {
+//            $auth = $this->get('app.auth');
+//
+//            /** @var User $user */
+//            $user = $auth->validateEntites('request', User::class, ['registration']);
+//            $password = $request->request->get('_password');
+//
+//            $user
+//                ->setPassword($passwordEncoder->encodePassword($user, $password));
+//
+//            $em->persist($user);
+//            $em->flush();
+//
+//            return $this->createSuccessResponse($user, ['profile'], true);
+//        } catch (ValidatorException $e) {
+//            $view = $this->view(['message' => $e->getErrorsMessage()], self::HTTP_STATUS_CODE_BAD_REQUEST);
+////            $logger->error($this->getMessagePrefix().'validate error: '.$e->getErrorsMessage());
+//        } catch (\Exception $e) {
+//            $view = $this->view((array) $e->getMessage(), self::HTTP_STATUS_CODE_BAD_REQUEST);
+////            $logger->error($this->getMessagePrefix().'error: '.$e->getMessage());
+//        }
+//
+//        return $this->handleView($view);
     }
 
     /**
